75c50b46ded21ffd042be0f0e0fa7dfd696d868f
Timothy Yoon <timsyoon@gmail.com>
2022-04-27 20:15:51 -0700

    Get code in current row and secret code from server
     - Add click listener to the check button
     - Get code entered in the current row
     - Define /secret-code route so client can access secret code

--- public/javascripts/mastermind.js
+++ public/javascripts/mastermind.js
@@ -1,7 +1,8 @@
-let secret_code;            // Sequence of 4 numbers
+let row_code = [];          // Sequence of 4 numbers
+let secret_code = [];
 let guessesRemaining = 10;
 let active_row = 0;
-let cracker_board = [];     // For storing the code cracker's responses & feedback
+let board = [];             // For storing the code cracker's responses & feedback
 
 // Board settings
 let num_rows = 10;          // 10 attempt rows
@@ -9,9 +10,40 @@ let num_cols = 5;           // 4 attempt columns + 1 feedback column
 
 // Initialize the board
 for (let i = 0; i < num_rows; i++) {
-  cracker_board.push(new Array(num_cols));
+  board.push(new Array(num_cols));
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    
+    let check_btn = document.querySelector('input.active');
+    check_btn.addEventListener('click', checkRow);
 });
+
+function checkRow() {
+    let row = this.parentNode.parentNode;
+    let cells = row.children;
+    row_code = [];  // Clear any previous code
+    // Iterate num_cols - 1 times because one column is for feedback
+    for (let i = 0; i < num_cols - 1; i++) {
+        let guess_val = cells[i].children[0].value;
+        if (guess_val == '') {
+            let other_feedback_div = document.getElementById('other-feedback');
+            other_feedback_div.innerText = 'Please enter numbers in all the text fields for the current row.';
+        } else {
+            row_code.push(guess_val);
+        }
+    }
+    // Check the current code against the secret code
+    axios
+      .get('/secret-code')
+      .then(resp => {
+          secret_code = resp.data;
+          if (_.isEqual(row_code, secret_code)) {
+              
+          } else {
+              
+          }
+      })
+      .catch(error => {
+          console.error(error);
+      });
+}
--- routes/index.js
+++ routes/index.js
@@ -18,10 +18,34 @@ let secret_code;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  generateRandomNumbers(res);
+  generateRandomNumbersAndRender(res);
 });
 
-function generateRandomNumbers(res) {
+router.get('/secret-code', function(req, res, next) {
+  if (secret_code != undefined) {
+    res.set('content-type', 'text/plain');
+    res.send(JSON.stringify(secret_code));
+  } else {
+    generateRandomNumbersAndRespond(res);
+  }
+});
+
+function generateRandomNumbersAndRespond(res) {
+  // Make an HTTP GET request to the Random Integer Generator
+  axios
+    .get(`https://www.random.org/integers/?num=${par_num}&min=${par_min}&max=${par_max}&col=${par_col}&base=${par_base}&format=${par_format}&rnd=${par_rnd}`)
+    .then(resp => {
+      secret_code = resp.data.trim().split('\n');
+      if (DEBUG) console.log(`secret_code = ${secret_code}`);
+      res.set('content-type', 'text/plain');
+      res.send(JSON.stringify(secret_code));
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function generateRandomNumbersAndRender(res) {
   // Make an HTTP GET request to the Random Integer Generator
   axios
     .get(`https://www.random.org/integers/?num=${par_num}&min=${par_min}&max=${par_max}&col=${par_col}&base=${par_base}&format=${par_format}&rnd=${par_rnd}`)
