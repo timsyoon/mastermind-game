1c27d97426b26ffff41bb8aab15d4ddc41b08d70
Timothy Yoon <timsyoon@gmail.com>
2022-04-27 23:00:40 -0700

    Generate feedback for guess

--- public/javascripts/mastermind.js
+++ public/javascripts/mastermind.js
@@ -59,6 +59,7 @@ function checkCodes(row_code, secret_code, current_btn) {
     if (_.isEqual(row_code, secret_code)) {
         other_feedback_div.innerText = 'You won';
     } else {
+        generateGuessFeedback(row_code, secret_code);
         if (current_row_index == 1) {
             other_feedback_div.innerText = 'You lost'
         } else {
@@ -66,6 +67,47 @@ function checkCodes(row_code, secret_code, current_btn) {
             current_row_index -= 1;
         }
     }
+}
+
+function generateGuessFeedback(row_code, secret_code) {
+    secret_code = ['4', '1', '4', '2'];
+    let feedback_array = [];  // 'o' for correct number & location, 'x' for only correct number
+    // Find counts of each number in the secret code
+    let map_secret = new Map();
+    for (let i = 0; i < secret_code.length; i++) {
+        let current_num = secret_code[i];
+        if (!map_secret.has(current_num)) {
+            map_secret.set(current_num, 1);
+        } else {
+            map_secret.set(current_num, map_secret.get(current_num) + 1);
+        }
+    }
+    // Populate the feedback array
+    // We will let exact matches (represented by 'o') take precedence over partial matches
+    for (let j = 0; j < row_code.length; j++) {
+        let current_num = row_code[j];
+        if (map_secret.has(current_num) && map_secret.get(current_num) > 0) {
+            if (current_num == secret_code[j]) {
+                feedback_array.push('o');
+                map_secret.set(current_num, map_secret.get(current_num) - 1);
+            }
+        }
+    }
+    // Find partial matches
+    for (let k = 0; k < row_code.length; k++) {
+        let current_num = row_code[k];
+        if (map_secret.has(current_num) && map_secret.get(current_num) > 0) {
+            if (current_num != secret_code[k]) {
+                feedback_array.push('x');
+                map_secret.set(current_num, map_secret.get(current_num) - 1);
+            }
+        }
+    }
+    while (feedback_array.length < 4) {
+        feedback_array.push('');
+    }
+    console.log(feedback_array);
+    // todo: scramble the feedback
 }
 
 function updateCheckButton(current_btn) {
